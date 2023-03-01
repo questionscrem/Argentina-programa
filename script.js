6a1bbe2a310466ac128cfa398a3c34ee8120d831
questionscrem <axel.pinto.ramos@gmail.com>
2023-02-28 22:52:26 -0300

    primer repositorio

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() { 
+		
+	animateprogress("#html5",91);
+	animateprogress("#php",72);
+	animateprogress("#css",86);
+	animateprogress("#python",52);
+	animateprogress("#javascript",79);
+	animateprogress("#nodejs",36);		
+}
+
+document.querySelector ("#boton").addEventListener ("click", function() { 
+	animateprogress("#html5",91);
+	animateprogress("#php",72);
+	animateprogress("#css",86);
+	animateprogress("#python",52);
+	animateprogress("#javascript",79);
+	animateprogress("#nodejs",36);
+});
